76fb82740ffde99e9d7b75c7d16e405d49cd7aff
NadeemA23 <n.akhtar50@outlook.com>
2025-04-28 11:00:42 +0000

    Prepare for Render deployment with PostgreSQL

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+
+
 
 # Define the Recipe model
 class Recipe(db.Model):
