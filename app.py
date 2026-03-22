f7533be54daea67c78b11e49c116425119395f49
NadeemA23 <105326841+NadeemA23@users.noreply.github.com>
2026-03-22 22:21:26 +0000

    Add User model and login loaded

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ login_manager.login_view = "login"
 
 
 # Define the Recipe model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 class Recipe(db.Model):
+    @login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
